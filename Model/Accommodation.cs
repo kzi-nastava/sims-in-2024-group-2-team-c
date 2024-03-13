9dabf23bd005f35ca4752d725d54daae66f19a3b
LEEEENCI <LEEEENCI@DESKTOP-G1LMPI1>
2024-03-13 17:07:54 +0100

    dodavanje Accommodation modela i repository i csv

---
+++ Model/Accommodation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Serializer;
+
+namespace BookingApp.Model
+{
+    public class Accommodation : ISerializable
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //public Location location{ get; set; }
+
+        public string Type { get; set; }
+        public int MaxGuests { get; set; }
+        public int MinBookingDays { get; set; }
+        public int CancellationDays { get; set; }
+        public List<string> Images { get; set; }
+
+        public Accommodation()
+        {
+            Images = new List<string>();
+        }
+
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Name, Type, MaxGuests.ToString(), MinBookingDays.ToString(), CancellationDays.ToString() };
+            foreach (string imagePath in Images)
+            {
+                csvValues.Append($"{imagePath};");
+            }
+            return csvValues;
+        }
+
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Name = values[1];
+            Type = values[2];  //bice pomereni svi za jedan kada dodamo Location, User = new User() { Id = Convert.ToInt32(values[3]) mozda ce se Lokacija praviti ovako isto jer nije int/string
+            MaxGuests = Convert.ToInt32(values[3]);
+            MinBookingDays = Convert.ToInt32(values[4]);
+            CancellationDays = Convert.ToInt32(values[5]);
+            Images = new List<string>();
+            for (int i = 6; i < values.Length; i++)
+            {
+                Images.Add(values[i]); // Dodajemo svaku putanju slike u listu Images
+            }
+        }
+    }
+}
---
+++ Repository/AccommodationRepository.cs
@@ -0,0 +1,69 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookingApp.Repository
+{
+    public class AccommodationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/accommodations.csv";
+        private readonly Serializer<Accommodation> _serializer;
+        private List<Accommodation> _accommodations;
+
+        public AccommodationRepository()
+        {
+            _serializer = new Serializer<Accommodation>();
+            _accommodations = _serializer.FromCSV(FilePath);
+        }
+
+        public List<Accommodation> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Accommodation Save(Accommodation accommodation)
+        {
+            accommodation.Id = NextId();
+            _accommodations = _serializer.FromCSV(FilePath);
+            _accommodations.Add(accommodation);
+            _serializer.ToCSV(FilePath, _accommodations);
+            return accommodation;
+        }
+
+        public int NextId()
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            if (_accommodations.Count < 1)
+            {
+                return 1;
+            }
+            return _accommodations.Max(a => a.Id) + 1;
+        }
+
+        public void Delete(Accommodation accommodation)
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            Accommodation founded = _accommodations.Find(a => a.Id == accommodation.Id);
+            _accommodations.Remove(founded);
+            _serializer.ToCSV(FilePath, _accommodations);
+        }
+
+        public Accommodation Update(Accommodation accommodation)
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            Accommodation current = _accommodations.Find(a => a.Id == accommodation.Id);
+            int index = _accommodations.IndexOf(current);
+            _accommodations.Remove(current);
+            _accommodations.Insert(index, accommodation);
+            _serializer.ToCSV(FilePath, _accommodations);
+            return accommodation;
+        }
+
+       // public List<Accommodation> GetByOwner(User owner)
+        //{
+            //_accommodations = _serializer.FromCSV(FilePath);
+            //return _accommodations.FindAll(a => a.Owner.Id == owner.Id);
+       // }
+    }
+}
