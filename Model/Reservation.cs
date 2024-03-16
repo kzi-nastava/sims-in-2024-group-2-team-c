45126a28c2a0b282edf045fecd408d41e7ef4c6e
alim344 <milabudimirovic2@gmail.com>
2024-03-16 19:49:15 +0100

    Dodavanje Reservation klase

---
+++ Model/Reservation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Serializer;
+
+namespace BookingApp.Model
+{
+    public class Reservation : ISerializable
+    {
+
+        public int Id { get; set; }
+        public int TourInstanceId;
+        public int TouristCount;
+        public List<int> OtherTouristIds;
+
+        public Reservation() { 
+            OtherTouristIds = new List<int>();
+        }
+
+        public Reservation(int id, int tourInstanceId, int touristCount, List<int> otherTourists)
+        {
+            Id = id;
+            TourInstanceId = tourInstanceId;
+            TouristCount = touristCount;
+            OtherTouristIds = otherTourists;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            TourInstanceId = Convert.ToInt32(values[1]);
+            TouristCount = Convert.ToInt32(values[2]);
+            OtherTouristIds = values[3].Split(',').Select(id => Convert.ToInt32(id)).ToList();
+        }
+    
+
+        public string[] ToCSV()
+        {
+            return new string[] { Id.ToString(), TourInstanceId.ToString(), TouristCount.ToString(), string.Join(",", OtherTouristIds) };
+        }
+    }
+}
---
+++ Repository/ReservationRepository.cs
@@ -0,0 +1,73 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BookingApp.Repository
+{
+    public class ReservationRepository
+    {
+
+        private const string FilePath = "../../../Resources/Data/reservations.csv";
+
+        private readonly Serializer<Reservation> _serializer;
+
+        private List<Reservation> _reservations;
+
+        public ReservationRepository()
+        {
+            _serializer = new Serializer<Reservation>();
+            _reservations = _serializer.FromCSV(FilePath);
+        }
+
+        public List<Reservation> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Reservation Save(Reservation reservation)
+        {
+            reservation.Id = NextId();
+            _reservations = _serializer.FromCSV(FilePath);
+            _reservations.Add(reservation);
+            _serializer.ToCSV(FilePath, _reservations);
+            return reservation;
+        }
+
+        public int NextId()
+        {
+            _reservations = _serializer.FromCSV(FilePath);
+            if (_reservations.Count < 1)
+            {
+                return 1;
+            }
+            return _reservations.Max(c => c.Id) + 1;
+        }
+
+        public void Delete(Reservation reservation)
+        {
+            _reservations = _serializer.FromCSV(FilePath);
+            Reservation founded = _reservations.Find(c => c.Id == reservation.Id);
+            _reservations.Remove(founded);
+            _serializer.ToCSV(FilePath, _reservations);
+        }
+
+        public Reservation Update(Reservation reservation)
+        {
+            _reservations = _serializer.FromCSV(FilePath);
+            Reservation current = _reservations.Find(c => c.Id == reservation.Id);
+            int index = _reservations.IndexOf(current);
+            _reservations.Remove(current);
+            _reservations.Insert(index, reservation);       // keep ascending order of ids in file 
+            _serializer.ToCSV(FilePath, _reservations);
+            return reservation;
+        }
+
+
+
+    }
+}
