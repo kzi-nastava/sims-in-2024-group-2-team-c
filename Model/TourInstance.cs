3be71147031985d07271c24552e2f5215f5c6c73
Aleksandra <aleksandrabegovic02@gmail.com>
2024-03-16 19:37:28 +0100

    Dodavanje instance ture

---
+++ Model/TourInstance.cs
@@ -0,0 +1,55 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace BookingApp.Model
+{
+    class TourInstance : ISerializable
+    {
+        public int Id { get; set; }
+        public int IdTour { get; set; }
+        public int MaxTourists { get; set; }
+        public int ReservedTourists { get; set; }
+        public bool Started { get; set; }
+        public bool Ended { get; set; }
+        public DateTime Date { get; set; }
+
+        public TourInstance() 
+        {
+            ReservedTourists = 0;
+            Started = false;
+            Ended = false;
+        }
+        public TourInstance(int idTour, int maxTourists, int reservedTourists, bool started, bool ended, DateTime date)
+        {
+            IdTour = idTour;
+            MaxTourists = maxTourists;
+            this.ReservedTourists = reservedTourists;
+            this.Started = started;
+            this.Ended = ended;
+            Date = date;
+        }
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            IdTour = Convert.ToInt32(values[1]);
+            MaxTourists = Convert.ToInt32(values[2]);
+            ReservedTourists = Convert.ToInt32(values[3]);
+            Started = Convert.ToBoolean(values[4]);
+            Ended = Convert.ToBoolean(values[5]);
+            Date = Convert.ToDateTime(values[6]);
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), IdTour.ToString(), MaxTourists.ToString(),
+                ReservedTourists.ToString(), Started.ToString(),Ended.ToString(),Date.GetDateTimeFormats().ToString() };
+            return csvValues;
+        }
+    }
+}
---
+++ Repository/TourInstanceRepository.cs
@@ -0,0 +1,68 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository
+{
+    class TourInstanceRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tourinstance.csv";
+
+        private readonly Serializer<TourInstance> _serializer;
+
+        private List<TourInstance> _tourInstances;
+
+        public TourInstanceRepository()
+        {
+            _serializer = new Serializer<TourInstance>();
+            _tourInstances = _serializer.FromCSV(FilePath);
+        }
+
+        public List<TourInstance> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public TourInstance Save(TourInstance tourInstance)
+        {
+            tourInstance.Id = NextId();
+            _tourInstances = _serializer.FromCSV(FilePath);
+            _tourInstances.Add(tourInstance);
+            _serializer.ToCSV(FilePath, _tourInstances);
+            return tourInstance;
+        }
+
+        public int NextId()
+        {
+            _tourInstances = _serializer.FromCSV(FilePath);
+            if (_tourInstances.Count < 1)
+            {
+                return 1;
+            }
+            return _tourInstances.Max(t => t.Id) + 1;
+        }
+
+        public void Delete(TourInstance tourInstance)
+        {
+            _tourInstances = _serializer.FromCSV(FilePath);
+            TourInstance founded = _tourInstances.Find(t => t.Id == tourInstance.Id);
+            _tourInstances.Remove(founded);
+            _serializer.ToCSV(FilePath, _tourInstances);
+        }
+
+        public TourInstance Update(TourInstance tourInstance)
+        {
+            _tourInstances = _serializer.FromCSV(FilePath);
+            TourInstance current = _tourInstances.Find(t => t.Id == tourInstance.Id);
+            int index = _tourInstances.IndexOf(current);
+            _tourInstances.Remove(current);
+            _tourInstances.Insert(index, tourInstance);
+            _serializer.ToCSV(FilePath, _tourInstances);
+            return tourInstance;
+        }
+    }
+}
