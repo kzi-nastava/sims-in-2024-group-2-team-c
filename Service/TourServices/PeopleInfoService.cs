38843c35c220d087a642ca049ee8daed048d5123
Aleksandra <aleksandrabegovic02@gmail.com>
2024-04-15 00:23:44 +0200

    Adding class PeopleInfoService

---
+++ Service/TourServices/PeopleInfoService.cs
@@ -0,0 +1,26 @@
+﻿using BookingApp.Interfaces;
+using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Service.TourServices
+{
+    public class PeopleInfoService
+    {
+        public PeopleInfoRepository _peopleInfoRepository { get; set; }
+
+
+        public PeopleInfoService()
+        {
+            _peopleInfoRepository = new PeopleInfoRepository();
+        }
+        public PeopleInfo GetById(int id)
+        {
+            return _peopleInfoRepository.GetById(id);
+        }
+    }
+}
