3c28e5f6a8360661d9f47d3625dd6ed2e1892da9
alim344 <milabudimirovic2@gmail.com>
2024-04-29 13:30:02 +0200

    Adding ConverterToUri and ViewModelLocator

---
+++ Converters/ObjectToUriConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BookingApp.Converters
+{
+    class ObjectToUriConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            return new Uri(value.ToString(), UriKind.Relative);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
